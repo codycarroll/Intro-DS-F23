4ce220d771ab346c4629c49b17f463e17404324f
Cody Carroll <cjcarroll@usfca.edu>
2023-09-28 11:25:33 -0700

    factors

--- CodeDemos/Factors.R
+++ CodeDemos/Factors.R
@@ -1,14 +1,23 @@
 ##Factors
 library(tidyverse)
 install.packages("forcats")
-library(forcats)
+library(forcats) #for categorical data
 
 ##Creating a factor vector for months of the year
-data = c("Jan", "Dec", "Apr", "Sep") #the data we'd like to make a factor
+data = c("Jan", "Dec", "Apr", "Sep") 
+#the data we'd like to make a factor
+
+# there are 2 small issues with raw vectors for this kind of data
+#1. there could be typos
+#ex c("Jam", "Dec", "Apr")
+#2. doesn't sort in a useful way
+sort(data)
+
+#Instead let's create a "Factor" which will help us with both:
 
 #a vector of all possible levels that data can take on
-month_levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
-                  "Aug", "Sep", "Oct", "Nov", "Dec")
+month_levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                 "Jul","Aug", "Sep", "Oct", "Nov", "Dec")
 
 #create the factor variable
 mon_factor = factor(data, levels = month_levels)
@@ -21,7 +30,6 @@ mon_factor_short = factor(data)
 
 #making a factor from data that is not included in the possible levels
 #will create an NA
-
 data2 = c("Jam", "Dec", "Apr", "Maz")
 mon_factor_NA = factor(data2, levels = month_levels)
 
@@ -43,10 +51,11 @@ levels(mon_factor_app)
 
 #overriding the current order of levels
 levels(mon_factor_app) = c("Dec", "Jan", "Sep", "Apr")
-
+sort(mon_factor_app)
 
 ##Playing around with a data set
 dataset = gss_cat
+?gss_cat
 str(dataset)
 
 #get an overall count of each level in a factor
@@ -55,10 +64,30 @@ count(dataset, race)
 
 count(dataset, denom)
 
+#alternatively use pipes:
+dataset %>% 
+  count(race)
 
-#Re-ordering a factor according to another variable
-#example - re-order the religion factor in order of the number of tvhours
-fct_reorder(dataset$relig, dataset$tvhours)
+#can sort by counts:
+dataset %>% 
+  count(race) %>% 
+  arrange(desc(n))
+
+dataset %>% 
+  count(denom) %>% 
+  arrange(desc(n))
+
+### Intro to Visualization
+
+#bar plot of race
+ggplot(gss_cat, aes(race)) +
+  geom_bar()
+
+#relative frequency
+ggplot(gss_cat, aes(race)) +
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  ylab("relative frequency")
+
 
 #creating a summary of religion according to tvhours
 relig_summary = gss_cat %>%
@@ -70,11 +99,19 @@ relig_summary = gss_cat %>%
   )
 
 #plot with factors not ordered
-ggplot(relig_summary, aes(tvhours, relig)) + geom_point()
+ggplot(relig_summary, aes(tvhours, relig)) + 
+  geom_point()
+
+
+
+#Re-ordering a factor according to another variable
 
 #plot with re-ordered factors in religion
 ggplot(relig_summary, aes(tvhours, fct_reorder(relig, tvhours))) +
   geom_point()
+
+#in general it's better to do the reordering/other transformations outside 
+# of the aes ... example below:
 
 
 ##re-naming levels using the fct_recode() function
@@ -97,9 +134,79 @@ count(mutate1, partyid)
 
 
 #collapsing a lot of levels to be more simple label names
-#here, we collapse multiple levels into the levels "other", "rep", "ind", and "dem"
+#here, we collapse multiple levels into the levels "other", 
+#"rep", "ind", and "dem"
 newpartyid = fct_collapse(dataset$partyid,
-                       other = c("No answer", "Don't know", "Other party"),
-                       rep = c("Strong republican", "Not str republican"),
-                       ind = c("Ind,near rep", "Independent", "Ind,near dem"),
-                       dem = c("Not str democrat", "Strong democrat"))
+                       other = c("No answer", 
+                                 "Don't know", 
+                                 "Other party"),
+                       rep = c("Strong republican", 
+                               "Not str republican"),
+                       ind = c("Ind,near rep", 
+                               "Independent", 
+                               "Ind,near dem"),
+                       dem = c("Not str democrat", 
+                               "Strong democrat"))
+
+dataset$party_summary = newpartyid
+
+
+
+###revisiting religious refactoring by tv hours using mutate
+
+relig_summary %>%
+  mutate(relig = fct_reorder(relig, tvhours)) %>%
+  ggplot(aes(tvhours, relig)) +
+  geom_point()
+
+
+#a similar plot focusing on income sorted by age:
+
+rincome_summary <- gss_cat %>%
+  group_by(rincome) %>%
+  summarise(
+    age = mean(age, na.rm = TRUE),
+    tvhours = mean(tvhours, na.rm = TRUE),
+    n = n()
+  )
+
+##Income Range ordered by Age
+ggplot(rincome_summary, aes(age, fct_reorder(rincome, age))) + 
+  geom_point()
+
+### Exercise
+#1. Make the same plot with mutate and pipes instead 
+# of transforming in the aes.
+
+#2. Is sorting income ranges by age a good idea? Why or why not?
+
+#3. What is the default order of the income range level?
+
+#4. If we want to preserve income range level order, but move NA to the end,
+ #. what would our plot look like? (Hint: you can use fct_relevel.)
+
+
+rincome_summary %>% 
+  mutate(rincome = fct_relevel(rincome, "Not applicable")) %>% 
+  ggplot(aes(age, rincome)) +
+  geom_point()
+
+
+
+### Extra features if time:
+
+by_age <- gss_cat %>%
+  filter(!is.na(age)) %>%
+  count(age, marital) %>%
+  group_by(age) %>%
+  mutate(prop = n / sum(n))
+
+ggplot(by_age, aes(age, prop, colour = marital)) +
+  geom_line(na.rm = TRUE)
+
+ggplot(by_age, aes(age, prop, colour = fct_reorder2(marital, age, prop))) +
+  geom_line() +
+  labs(colour = "marital")
+
+
+
